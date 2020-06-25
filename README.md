2f7d6ad1eaf3ae1f8a63d5a91db2ccb5de7465d6
eklem1 <emma.klemets@mail.mcgill.ca>
2020-06-25 16:42:36 -0400

    added function to get masses for specific redshifts, looking at why the smf fits suck

--- README.md
+++ README.md
@@ -10,7 +10,10 @@ pop = ares.populations.GalaxyPopulation(**pars)
 **WorkingFromHalo.ipynb**: Looking at a simplified relation of the HMF to LF as well as to SMF.   
 **HMFtoSMF_nonLinear.ipynb**: Starting from Moster2010 paper, a DPL relation of HMF to SMF.  
 **SFRs.ipynb**: Starting from Speagle2014 paper, smain sequence SFR, SSFR and SFRD.  
-**FirstFits**: Starting to work with fitting in ARES.
+
+**FirstFits.ipynb**: Starting to work with fitting in ARES for the LF.  
+**SMF_fit.ipynb**: Working on MCMC fit of SMF, currently not looking too great.  
+**SMF_fitScript.py**: Python script of SMF_fit.ipynb.  
  
 **DataCompile.ipynb**: Script to read and format data from "behroozi-2013-data-compilation" for more SMF and SSFR lit values.
 
---
+++ SMF_fitScript.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+import ares
+import numpy as np
+import matplotlib.pyplot as pl
+import distpy
+
+#could add sources so only those used to fit are considered
+def getMasses(redshifts):
+    data = ares.analysis.GalaxyPopulation()
+
+    Ms = []
+    for z in redshifts:
+        data_info = data.compile_data(z, quantity='smf')
+
+        # print(data_info)
+
+        mass = []
+        for i in data_info:
+    #         mass.append(data_info[i]["M"][~data_info[i]["M"].mask])
+            mass.extend(data_info[i]["M"].compressed())
+
+        Ms.extend(mass)
+
+    Ms = list(set(Ms))
+    Ms = np.sort(Ms)
+    return Ms
+
+# Independent variables
+redshifts = np.sort(np.array([0.35, 0.875, 1.125, 1.75, 2.25, 2.75, 1.65, 2.5, 3.5, 0.10165, 0.25, 0.45, 0.575, 0.725, 0.9]))
+
+
+# [0.10165, 0.25, 0.35, 0.45, 0.575, 0.725, 0.9][1.65, 2.5, 3.5]
+#Ms = np.logspace(7, 12, 50)
+Ms = getMasses(redshifts)
+
+# blob 1: the LF. Give it a name, and the function needed to calculate it.
+blob_n1 = ['galaxy_smf']
+blob_i1 = [('z', redshifts), ('bins', Ms)]
+blob_f1 = ['StellarMassFunction']
+
+blob_pars = \
+{
+ 'blob_names': [blob_n1],
+ 'blob_ivars': [blob_i1],
+ 'blob_funcs': [blob_f1],
+ 'blob_kwargs': [None],
+}
+
+#define the parameters that remain unchanged
+base_pars = ares.util.ParameterBundle('emma:model1')
+base_pars.update(blob_pars)
+base_pars.update({'progress_bar': True, 'debug':True})
+
+free_pars = \
+[
+    'pq_func_par0[0]',
+    'pq_func_par2[0]', 
+
+    #norm
+    'pq_func_par0[1]',
+    'pq_func_par2[1]', 
+
+    #gamma
+    'pq_func_par0[2]',
+    'pq_func_par2[2]', 
+
+    #peak mass
+    'pq_func_par0[3]',
+    'pq_func_par2[3]', 
+]
+
+is_log = [False, False, False, False, False, False, False, False]
+
+from distpy.distribution import UniformDistribution
+from distpy.distribution import DistributionSet
+
+ps = DistributionSet()
+ps.add_distribution(UniformDistribution(0, 4), 'pq_func_par0[0]')
+ps.add_distribution(UniformDistribution(-1, 1),  'pq_func_par2[0]')
+
+ps.add_distribution(UniformDistribution(0.001, 2),   'pq_func_par0[1]')
+ps.add_distribution(UniformDistribution(-1, 1),  'pq_func_par2[1]')
+
+ps.add_distribution(UniformDistribution(0, .9),   'pq_func_par0[2]')
+ps.add_distribution(UniformDistribution(-3, -0.01),  'pq_func_par2[2]')
+
+ps.add_distribution(UniformDistribution(10.0, 14.0),   'pq_func_par0[3]')
+ps.add_distribution(UniformDistribution(0, 2),  'pq_func_par2[3]')
+
+#From Moster2010, table 7
+logM_0 = 11.88 #(0.01)
+mu = 0.019 #(0.002)
+N_0 = 0.0282 #(0.0003)
+nu = -0.72 #(0.06)
+gamma_0 = 0.556 #0.001
+gamma_1 = -0.26 #(0.05)
+beta_0 = 1.06 #(0.06)
+beta_1 = 0.17 #(0.12)
+
+guesses = \
+{
+    'pq_func_par0[0]': beta_0,
+    'pq_func_par2[0]': beta_1, 
+
+    #norm
+    'pq_func_par0[1]': N_0,
+    'pq_func_par2[1]': nu, 
+
+    #gamma
+    'pq_func_par0[2]': gamma_0,
+    'pq_func_par2[2]': gamma_1, 
+
+    #peak mass
+    'pq_func_par0[3]': logM_0,
+    'pq_func_par2[3]': mu, 
+}
+
+# Initialize a fitter object and give it the data to be fit
+fitter_smf = ares.inference.FitGalaxyPopulation(**base_pars)
+
+fitter_smf.include.append('smf')
+
+# The data can also be provided more explicitly
+fitter_smf.data = 'tomczak2014',  'mortlock2011', 'moustakas2013', 'marchesini2009_10'
+
+fitter = ares.inference.ModelFit(**base_pars)
+fitter.add_fitter(fitter_smf)
+
+# Establish the object to which we'll pass parameters
+from ares.populations.GalaxyHOD import GalaxyHOD
+fitter.simulator = GalaxyHOD
+
+fitter.save_hmf = True  # cache HMF for a speed-up!
+fitter.save_psm = True  # cache source SED model (e.g., BPASS, S99)
+
+# Setting this flag to False will make ARES generate new files for each checkpoint.
+# 2-D blobs can get large, so this allows us to just download a single
+# snapshot or two if we'd like (useful if running on remote machine)
+fitter.checkpoint_append = False
+
+fitter.parameters = free_pars
+fitter.is_log = is_log
+fitter.prior_set = ps
+
+# In general, the more the merrier (~hundreds)
+fitter.nwalkers = 70
+
+# fitter.jitter = [0.1] * len(fitter.parameters)
+fitter.jitter = [0.1, 0.1, 0.01, 0.05, 0.1, 0.1, 0.8, 0.1]
+
+fitter.guesses = guesses
+# fitter.debug('True')
+# fitter.pops
+
+# Run the thing
+fitter.run('smf_run3', burn=20, steps=120, save_freq=5, clobber=True)
